7f8d6b7e16b1aee65ff0e7fbdd314057b23aa88b
zhzh <white.three@outlook.com>
2023-09-20 18:11:54 +0800

    haskell函数写完

---
+++ c/swap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void swap(int* a,int* b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+int main(){
+    int a = 1;
+    int b = 2;
+    swap(&a,&b);
+    printf("a = %d,b = %d",a,b);
+    return 0;
+}
